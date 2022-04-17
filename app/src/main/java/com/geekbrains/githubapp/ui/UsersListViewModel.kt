e3d678a4fc942e2f620b51259c1e5e4c8aed6a03
NelsonBeard <ilnur111111@yandex.ru>
2022-04-17 19:16:46 +0500

    Вывел список пользователей на экран

--- app/src/main/java/com/geekbrains/githubapp/ui/UsersListViewModel.kt
+++ app/src/main/java/com/geekbrains/githubapp/ui/UsersListViewModel.kt
@@ -1,4 +1,18 @@
 package com.geekbrains.githubapp.ui
 
-class UsersListViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.geekbrains.githubapp.data.MockUsersRepoImpl
+import com.geekbrains.githubapp.domain.User
+
+class UsersListViewModel(
+    private val liveDataForViewToObserve: MutableLiveData<List<User>> = MutableLiveData()
+) : ViewModel() {
+
+    fun getData(): LiveData<List<User>>{
+        liveDataForViewToObserve.value = MockUsersRepoImpl().getUsersList()
+        return liveDataForViewToObserve
+    }
+
 }
